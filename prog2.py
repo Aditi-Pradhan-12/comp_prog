fc157a3c4c8bc7ca13afb6773084f66e83df4c06
Aditi Pradhan <aditi.pradhan1210@gmail.com>
2025-01-18 10:17:54 +0530

    prog 2.1

--- prog2.py
+++ prog2.py
@@ -1,5 +1,5 @@
 #Write a Python code to concatenate two strings
 str1 = str(input("enter the string 1: "))
 str2 = str(input("enter the string 2: "))
-result = str1 + str2
+result = str1 + " " + str2
 print(f"the expected string is: {result}")
