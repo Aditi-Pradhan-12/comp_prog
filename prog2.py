bc7e40b12ceebb46cd93bd2f99c3a1e5f8a436a3
Aditi Pradhan <aditi.pradhan1210@gmail.com>
2025-01-18 14:23:09 +0530

    prog 2.0

--- prog2.py
+++ prog2.py
@@ -1,5 +1,30 @@
 #Write a Python code to concatenate two strings
-str1 = str(input("enter the string 1: "))
+'''str1 = str(input("enter the string 1: "))
 str2 = str(input("enter the string 2: "))
 result = str1 + " " + str2
-print(f"the expected string is: {result}")
+print(f"the expected string is: {result}")'''
+
+#Write a Python program to count the number of vowels in a string
+'''s = str(input("enter the string: "))
+l = ['a','e','i','o','u','A','E','I','O','U']
+count = 0
+for char in s:
+    if char in l:
+        count = count + 1
+    else:
+        continue
+print(f'the number of vowels are: {count}')'''
+
+#Write a Python program to calculate the factorial of a number
+'''fact = 1
+num = int(input("enter the number: "))
+i = 1 
+for i in range (1, num+1):
+    fact = fact * i  
+print(f'the factorial of the number is: {fact}')'''
+
+#Write a Python program to calculate the area of a rectangle
+'''len = int(input("enter the length of rectangle: "))
+br = int(input("enter the breadth of the rectangle: "))
+area = len * br 
+print(f'the area of rect is: {area}')'''
